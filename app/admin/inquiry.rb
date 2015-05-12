35089b799dc0b2d25cd644b925afbfb1c641b70a
volx757 <bendbarclay@gmail.com>
2015-05-12 00:23:59 -0400

    qa

---
+++ app/admin/inquiry.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Inquiry do
+
+  actions :all, :except => [:new, :create, :destroy, :show, :edit, :update]
+
+  filter :name
+  filter :email
+
+  config.per_page = 20
+  config.sort_order = "id_asc"
+  config.batch_actions = false
+
+  index do
+    column :id
+    column :name
+    column :email
+    column :message
+  end
+
+end 
