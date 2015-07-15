e015e40a5963184f95ed5c0a2d636ee69e2d323c
volx757 <bendbarclay@gmail.com>
2015-07-14 23:52:27 -0400

    qa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?, :guest?
+  helper_method :current_user, :logged_in?, :guest?, :is_mobile?
 
   def current_user
     @current_user ||= (User.find(session[:user_id]) if session[:user_id]) || (Guest.where('session_id = ?', session[:guest_key]).last if session[:guest_key])
@@ -16,6 +16,13 @@ class ApplicationController < ActionController::Base
 
   def guest?
     current_user.is_guest?(session[:user_id])
+  end
+
+  def is_mobile?
+    (request.user_agent.present? && (request.user_agent.include?('iPhone') || request.user_agent.include?('Android') ||
+        request.user_agent.include?('Windows Phone') || request.user_agent.include?('iPad'))) ?
+        true :
+        false
   end
 
 
